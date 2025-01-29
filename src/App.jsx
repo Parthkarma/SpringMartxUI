346b7f7fc683bb120326ba205af684ede0e4e252
parth karma <codeparthk@gmail.com>
2025-01-29 18:35:06 +0530

    setting up dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {FaBeer} from 'react-icons/fa'
+function App() {
+  return (
+    <>     
+    <div className='flex items-center justify-center h-screen bg-gray-800 text-white  text-2xl font-bold'>
+      Welcome  to react page  Building the ecommerce frontend page  <FaBeer/>
+    </div>
+    </>
+  )
+}
+export default App
