042ccd5eda689746871ce34b5ed36ae7da5cc552
parth karma <codeparthk@gmail.com>
2025-01-30 16:03:09 +0530

    building product Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
-import {FaBeer} from 'react-icons/fa'
+import Products  from './components/Products'
 function App() {
   return (
     <>     
-    <div className='flex items-center justify-center h-screen bg-gray-800 text-white  text-2xl font-bold'>
-      Welcome  to react page  Building the ecommerce frontend page  <FaBeer/>
-    </div>
-    </>
+  
+    <Products />
+   
+    </> 
   )
 }
 export default App
---
+++ src/components/ProductCart.jsx
@@ -0,0 +1,36 @@
+const ProductCart = ({
+    productId,
+    productName,
+    image,
+    description,
+    quantity,
+    price,
+    discount , 
+    specialPrice,
+    
+}) => {
+    const [openProductViewModal , setOpenProductViewModal] = useState(false);
+    const btnLoader = false ; 
+    const [selectedViewProduct , setSeletectedViewProduct] = useState("") ; 
+
+    const isAvailable = quantity && Number(quantity) > 0 ; 
+
+    return (
+        <div className ="border rounded-lg shadow-xl overflow-hidden transition-shadow duration-300">
+              <div onClick={() => {}} className = "w-full overflow-hidden aspect-[3/2]">
+                      <img 
+                      className="w-full h-full cursor-pointer transition-transform duration-300 transform hover:scale-105"
+                      src={image}
+                        alt= {productName} >                                      
+                      </img>
+              </div>
+          <div className ="p-4">
+            <h2 onClick={() => {}}
+               className="text-lg font-semibold mb-2 cursor-pointer" >
+                {productName}
+            </h2>
+          </div>
+        </div>
+    )
+}
+export default ProductCart;
